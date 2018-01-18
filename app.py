f7fd2e2bf9d2284d3b0f861ad40fa5c35a28e4d5
jorge oviedo <jorgeloviedo@hotmail.com>
2018-01-18 17:10:46 -0300

    update

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def hello():
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>version: latest</b>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
